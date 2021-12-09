f855ba650786d3aef6d6b86d87b3fca1bdb1c0fc
Yulia Osadchuk <yulia.zelenskaya@gmail.com>
2021-12-09 11:46:19 +0200

    resolve merge conflicts

--- src/components/signin/sign-in-lazy.tsx
+++ src/components/signin/sign-in-lazy.tsx
@@ -3,6 +3,6 @@ import { FC, lazy, LazyExoticComponent } from 'react';
 import { routeWithSuspense } from '../../route-with-suspense';
 import Loader from '../loader/loader';
 
-const SigninLazy: LazyExoticComponent<FC> = lazy(() => import('./signin'));
+const SigninLazy: LazyExoticComponent<FC> = lazy(() => import('./sign-in'));
 
 export default routeWithSuspense(SigninLazy, Loader);
