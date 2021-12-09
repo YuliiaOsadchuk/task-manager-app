5a8d230879b436ab8693d8472551fde74d8fd06a
Yulia Osadchuk <yulia.zelenskaya@gmail.com>
2021-12-09 11:40:03 +0200

    rename files src/pages/home/home-page.tsx src/components/signin/sign-in.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Signin from './components/signin/signin';
+import Signin from './components/signin/sign-in';
 import { APP_ROUTES } from './enums';
-import Home from './pages/home/home';
+import Home from './pages/home/home-page';
 
 const Router: React.FC = () => (
   <BrowserRouter>
