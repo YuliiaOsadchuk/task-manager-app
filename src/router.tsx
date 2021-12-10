7cc1d40944d2578b2807741cd051317bea9ba49c
Yulia Osadchuk <yulia.zelenskaya@gmail.com>
2021-12-10 13:32:07 +0200

    code fixes

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { APP_ROUTES } from './enums';
 import HomeLazy from './pages/home/home-lazy';
 import SigninLazy from './pages/sign-in/sign-in-lazy';
-import { APP_ROUTES } from './enums';
 
 const Router: React.FC = () => (
   <BrowserRouter>
